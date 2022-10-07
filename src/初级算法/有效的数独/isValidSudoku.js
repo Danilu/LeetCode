b8772116b041122f56182e04faa5f765746e5c87
Gin <1581312953@qq.com>
2022-10-07 11:34:33 +0800

    C++ 有效的数独 回溯

--- src/初级算法/有效的数独/isValidSudoku.js
+++ src/初级算法/有效的数独/isValidSudoku.js
@@ -1,3 +1,28 @@
 /**
  * @param url:https://leetcode.cn/problems/valid-sudoku/
  */
+
+ var isValidSudoku = function (board) {
+    // 回溯
+    // 行 列 3*3 分别判重
+    let rows = {};
+    let columns = {};
+    let boxes = {};
+    // 对数独遍历
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            // 有效数字
+            let num = board[i][j];
+            if (num != '.') {
+                let boxIndex = parseInt((i / 3)) * 3 + parseInt(j / 3);
+                if (rows[i + '-' + num] || columns[j + '-' + num] || boxes[boxIndex + '-' + num]) {//重复检测
+                    return false;
+                }
+                rows[i + '-' + num] = true;
+                columns[j + '-' + num] = true;
+                boxes[boxIndex + '-' + num] = true;
+            }
+        }
+    }
+    return true;
+}
